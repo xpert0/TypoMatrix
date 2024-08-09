5d572c8d23efe0976cd72fdcc5b8da5df1d67959
xpert0 <github.jaivs@slmail.me>
2024-08-09 16:37:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const translateForm = document.getElementById('translate-form');
-const hindiTranslationElement = document.getElementById('hindi-translation');
-
-// Load Hindi translations from JSON file
-const hindiTranslations = {};
-fetch('lang/hi.json')
-   .then(response => response.json())
-   .then(data => hindiTranslations = data);
-
-translateForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const englishWord = document.getElementById('english-word').value;
-    const hindiTranslation = hindiTranslations[englishWord];
-    if (hindiTranslation) {
-        hindiTranslationElement.innerText = hindiTranslation;
-    } else {
-        hindiTranslationElement.innerText = 'Translation not found';
-    }
-});
-
 const translate = require('google-translate-api');
 
 translate('Ik spreek Engels', {to: 'en'}).then(res => {
