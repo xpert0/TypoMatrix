9dcaec13031209d7ee8ac425a7a55ac65604f31e
xpert0 <github.jaivs@slmail.me>
2024-08-09 11:07:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ translateForm.addEventListener('submit', (e) => {
         hindiTranslationElement.innerText = 'Translation not found';
     }
 });
+
+const translate = require('google-translate-api');
+
+translate('Ik spreek Engels', {to: 'en'}).then(res => {
+    console.log(res.text);
+    //=> I speak English
+    console.log(res.from.language.iso);
+    //=> nl
+}).catch(err => {
+    console.error(err);
+});
