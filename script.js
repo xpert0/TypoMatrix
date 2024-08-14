ac26f6cc25580dd4a81dba1b1992244004685118
xpert0 <github.jaivs@slmail.me>
2024-08-14 22:17:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-function translate(){
-    const axios = require('axios').default;
-    const { v4: uuidv4 } = require('uuid');
+const englishText = document.getElementById('englishText');
+const hindiText = document.getElementById('hindiText');
+const translateBtn = document.getElementById('translateBtn');
 
-    let key = "395fd6e90529421ebb3a4c99486e77f7";
-    let endpoint = "https://api.cognitive.microsofttranslator.com";
+const apiKey = '395fd6e90529421ebb3a4c99486e77f7'; // Replace with your actual API key
+const endpoint = 'https://api.cognitive.microsofttranslator.com'; // Replace with your actual endpoint
 
-    // location, also known as region.
-    // required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
-    let location = "centralindia";
-
-    axios({
-        baseURL: endpoint
-        url: '/translate'
-        method: 'post'
+translateBtn.addEventListener('click', () => {
+    const textToTranslate = englishText.value;
+    
+    fetch(`${endpoint}/translate?api-version=3.0&from=en&to=hi`, {
+        method: 'POST',
         headers: {
-            'Ocp-Apim-Subscription-Key': key
-             // location required if you're using a multi-service or regional (not global) resource.
-            'Ocp-Apim-Subscription-Region': location
-            'Content-type': 'application/json'
-            'X-ClientTraceId': uuidv4().toString()
-        }
-        params: {
-            'api-version': '3.0'
-            'from': 'en'
-            'to': ['hi']
-        }
-        data: [{
-            'text': 'I would really like to drive your car around the block a few times!'
-        }]
-        responseType: 'json'
-    }).then(function(response){
-        console.log(JSON.stringify(response.data, null, 4));
+            'Ocp-Apim-Subscription-Key': apiKey,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify([{ Text: textToTranslate }])
     })
-}
+    .then(response => response.json())
+    .then(data => {
+        hindiText.value = data[0].translations[0].text;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
